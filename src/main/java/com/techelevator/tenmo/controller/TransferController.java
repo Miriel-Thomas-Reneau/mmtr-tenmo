d68e7b446338c1fdc020aed3b584dd4d349db5d6
Ankan Bhattacharya <ankanb240@gmail.com>
2025-07-10 12:57:56 -0400

    worked on TransferController

--- src/main/java/com/techelevator/tenmo/controller/TransferController.java
+++ src/main/java/com/techelevator/tenmo/controller/TransferController.java
@@ -7,9 +7,14 @@ import com.techelevator.tenmo.exception.DaoException;
 import com.techelevator.tenmo.model.TenmoAccount;
 import com.techelevator.tenmo.model.Transfer;
 import com.techelevator.tenmo.model.User;
+import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.parameters.P;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
@@ -60,4 +65,54 @@ public class TransferController {
         }
         return transfers;
     }
+
+    @PreAuthorize("isAuthenticated()")
+    @GetMapping(path = "/transfer/{transfer_id}")
+    public Transfer getTransfer(@PathVariable(name = "transfer_id") int transferId,
+                                Principal principal) {
+        Transfer transfer = null;
+        try {
+            boolean canSeeTransfer;
+            String username = principal.getName();
+            User user = userDao.getUserByUsername(username);
+            if (user == null) {
+                throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "User " + username + " does not exist");
+            }
+            int userId = user.getId();
+            transfer = transferDao.getTransferById(transferId);
+            canSeeTransfer = user.getRole().equals("ROLE_ADMIN") || transfer.getSenderAccountId() == userId
+                || transfer.getRecipientAccountId() == userId;
+            if (!canSeeTransfer) {
+                throw new ResponseStatusException(HttpStatus.FORBIDDEN,
+                        "Unauthorized to access transfer id " + transferId);
+            }
+        } catch (DaoException e) {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, e.getMessage());
+        }
+        return transfer;
+    }
+
+//    @PreAuthorize("hasRole('ROLE_USER')")
+//    @PutMapping(path = "/transfer/{transfer_id}")
+//    public Transfer putTransfer(@PathVariable(name = "transfer_id") int transferId,
+//                                @Valid @RequestBody Transfer transfer,
+//                                Principal principal) {
+//        try {
+//            String username = principal.getName();
+//            User user = userDao.getUserByUsername(username);
+//            if (user == null) {
+//                throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "User " + username + " does not exist");
+//            }
+//            int userId = user.getId();
+//            TenmoAccount tenmoAccount = tenmoAccountDao.getTenmoAccountByUserId(userId);
+//            if (tenmoAccount == null) {
+//                throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "User does not have a Tenmo account");
+//            }
+//            int tenmoAccountId = tenmoAccount.getTeAccountId();
+//            boolean isOwnTransfer = transfer.getSenderAccountId() == tenmoAccountId
+//                    ||
+//        } catch (DaoException e) {
+//            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, e.getMessage());
+//        }
+//    }
 }
