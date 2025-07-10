3fed9100303e8c2f74a0b5bae5c40ea3b3fc4e6e
Ankan Bhattacharya <ankanb240@gmail.com>
2025-07-10 11:54:43 -0400

    work on UserController

--- src/main/java/com/techelevator/tenmo/controller/UserController.java
+++ src/main/java/com/techelevator/tenmo/controller/UserController.java
@@ -5,8 +5,10 @@ import com.techelevator.tenmo.dao.interfaces.UsdAccountDao;
 import com.techelevator.tenmo.dao.interfaces.UserDao;
 import com.techelevator.tenmo.exception.DaoException;
 import com.techelevator.tenmo.model.TenmoAccount;
+import com.techelevator.tenmo.model.UsdAccount;
 import com.techelevator.tenmo.model.User;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
@@ -26,6 +28,8 @@ public class UserController {
         this.usdAccountDao = usdAccountDao;
     }
 
+    //TODO fix this to account for admin consuming this endpoint
+    @PreAuthorize("isAuthenticated()")
     @GetMapping(path = "/balance/tenmo")
     public BigDecimal getTenmoAccountBalance(Principal principal) {
         BigDecimal balance = null;
@@ -43,6 +47,7 @@ public class UserController {
         return balance;
     }
 
+    @PreAuthorize("hasRole('ROLE_USER')")
     @GetMapping(path = "/balance/usd")
     public BigDecimal getUsdAccountBalance(Principal principal) {
         BigDecimal balance = null;
@@ -53,7 +58,8 @@ public class UserController {
                 throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "User " + username + " does not exist");
             }
             int userId = user.getId();
-            // TODO finish getting USD balance; waiting on more UsdAccountDao methods
+            UsdAccount usdAccount = usdAccountDao.pullAccountInformation(userId);
+            balance = usdAccount.getUsdBucksBalance();
         } catch (DaoException e) {
             throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "DAO error - " + e.getMessage());
         }
